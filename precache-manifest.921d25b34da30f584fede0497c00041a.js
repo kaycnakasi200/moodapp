af29cc53ea5bf0177a38457f84fa7f47b3a09129
unknown <kaycnakasi200@gmail.com>
2019-08-28 10:29:51 +0700

    Updates

---
+++ precache-manifest.921d25b34da30f584fede0497c00041a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d9fd03a9f4217448f7200b4b98459d",
+    "url": "/moodapp/index.html"
+  },
+  {
+    "revision": "a249fdabd776502d538b",
+    "url": "/moodapp/static/css/main.a9edeffc.chunk.css"
+  },
+  {
+    "revision": "26b52edd886bea03a15c",
+    "url": "/moodapp/static/js/2.9f1712b0.chunk.js"
+  },
+  {
+    "revision": "a249fdabd776502d538b",
+    "url": "/moodapp/static/js/main.c4b79b12.chunk.js"
+  },
+  {
+    "revision": "dc16dae1c116239b434a",
+    "url": "/moodapp/static/js/runtime~main.d742958c.js"
+  }
+]);
